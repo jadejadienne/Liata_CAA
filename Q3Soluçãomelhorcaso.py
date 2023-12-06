69fd89c5b97b3c7b37e101c71fc896fe0ab7dbdc
Jadienne <117533327+jadejadienne@users.noreply.github.com>
2023-12-06 16:12:18 -0300

    Create Q3Soluçãomelhorcaso.py

---
+++ Q3Soluçãomelhorcaso.py
@@ -0,0 +1,16 @@
+def two_sum_best_case(nums, target):
+    num_indices = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_indices:
+            return [num_indices[complement], i]
+        num_indices[num] = i
+
+
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(two_sum_best_case(nums1, target1)) 
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(two_sum_best_case(nums2, target2))  
